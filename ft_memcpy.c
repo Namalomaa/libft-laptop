bb75da1e9cec0ed358a9e26be056278027ba9e78
ameer <nammora2005@gmail.com>
2025-11-25 20:43:23 +0300

    ft_memcpy added

---
+++ ft_memcpy.c
@@ -0,0 +1,11 @@
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*cdest;
+	const unsigned char	*csrc;
+
+	cdest = (unsigned char *)dest;
+	csrc = (const unsigned char *)src;
+	while (n--)
+		*cdest++ = *csrc++;
+	return (dest);
+}
