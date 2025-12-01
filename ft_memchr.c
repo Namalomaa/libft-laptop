8e1255dfc5786fa247242e7533d22ed0915d0ed7
ameer <nammora2005@gmail.com>
2025-12-01 20:45:04 +0300

    'memchr 1st version

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((void *)((unsigned char *)s + i));
+		i ++;
+	}
+	return (NULL);
+}
+
+#include <string.h>
+
+int main(void)
+{
+    const char str[] = "ABCDEFG";
+    const int chars[] = {'D', 'd'};
+    for (size_t i = 0; i < sizeof chars / (sizeof chars[0]); ++i)
+    {
+        const int c = chars[i];
+        const char *ps = ft_memchr(str, c, strlen(str));
+        ps ? printf ("character '%c'(%i) found: %s\n", c, c, ps)
+           : printf ("character '%c'(%i) not found\n", c, c);
+    }
+    return 0;
+}
