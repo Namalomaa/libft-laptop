858fc329be12e2d982be35bfc6a06b166faaf1d0
ameer <nammora2005@gmail.com>
2025-12-02 23:59:05 +0300

    special pointer for the edge case where n = 0

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
+#include "libft.h"
+
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*res;
+	size_t	total;
 
 	if (nmemb != 0 && (nmemb * size) / nmemb != size)
 		return (NULL);
-	res = malloc(nmemb * size);
+	total = nmemb * size;
+	if (total == 0)
+		total = 1;
+	res = malloc(total);
 	if (! res)
 		return (NULL);
-	ft_bzero (res, (nmemb * size));
+	ft_bzero (res, total);
 	return (res);
 }
