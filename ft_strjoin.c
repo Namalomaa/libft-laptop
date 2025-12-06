13dd7e58dbad25827301fb074b1be5cafb27b9d4
ameer <nammora2005@gmail.com>
2025-12-06 21:58:27 +0300

    new version of strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -15,27 +15,16 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*join;
-	size_t	size;
-	size_t	i;
-	size_t	j;
+	size_t	l1;
+	size_t	l2;
 
-	size = ft_strlen(s1) + ft_strlen(s2);
-	join = (char *)malloc(size + 1);
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	join = (char *)malloc(l1 + l2 + 1);
 	if (!join)
 		return (NULL);
-	i = 0;
-	while (i < ft_strlen(s1))
-	{
-		join[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (j < ft_strlen(s2))
-	{
-		join[i] = s2[j];
-		i++;
-		j++;
-	}
-	join[i] = '\0';
+	ft_memcpy(join, s1, l1);
+	ft_memcpy(join + l1, s2, l2);
+	join[l1 + l2] = '\0';
 	return (join);
 }
